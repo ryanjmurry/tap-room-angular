79de4adb6efdde2c1f1514e6c69f2f885d2e3be5
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-15 15:35:12 -0700

    Import keg class and mock keg list, create method to edit keg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model';
+import { KEGS } from './models/mock-kegs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tap Room';
+
+  masterKegList: Keg[] = KEGS;
+  selectedKeg = null;
+
+  editKeg(clickedKeg) {
+    this.selectedKeg = clickedKeg;
+  }
 }
