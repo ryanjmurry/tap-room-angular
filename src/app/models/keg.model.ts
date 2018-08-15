6a3a4405d598f4c2376dac89169a007e3ed0a257
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-15 15:33:25 -0700

    Create keg model and mock kegs

---
+++ src/app/models/keg.model.ts
@@ -0,0 +1,4 @@
+export class Keg {
+  public pintsRemaining: number = 124;
+  constructor(public beerName: string, public brewery: string, public price: number, public abv: number) { }
+}
---
+++ src/app/models/mock-kegs.ts
@@ -0,0 +1,8 @@
+import { Keg } from './keg.model';
+//constructor(public beerName: string, public brewery: string, public price: number, public abv: number
+export const KEGS: Keg[] = [
+  new Keg("Lagunitas IPA", "Lagunitas", 5, 6.2),
+  new Keg("Fresh Squeezed IPA", "Deschutes", 6, 6.4),
+  new Keg("Rainier Lager", "Rainier Brewing", 3, 4.6),
+  new Keg("Oatmeal Stout", "Firestone Walker", 7, 5.5)
+ ];
