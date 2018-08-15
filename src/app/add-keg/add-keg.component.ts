c77df04f8b1fa7cae34038fcc802377dd8078f11
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-15 16:31:04 -0700

    add add-keg component, add new keg input field and output event binding

---
+++ src/app/add-keg/add-keg.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Keg } from '../models/keg.model';
+
+@Component({
+  selector: 'app-add-keg',
+  templateUrl: './add-keg.component.html',
+  styleUrls: ['./add-keg.component.css']
+})
+export class AddKegComponent {
+  @Input() newKegForm: boolean;
+  @Output() submittedKeg = new EventEmitter();
+
+  submitForm(beerName: string, brewery: string, price: string, abv: string) {
+    let newPrice = +parseFloat(price).toFixed(2);
+    let newAbv = +parseFloat(abv).toFixed(2);
+    let newKeg = new Keg(beerName, brewery, newPrice, newAbv);
+    this.submittedKeg.emit(newKeg);
+  }
+
+  constructor() { }
+
+}
