a369beba203bed8a249e15ecdbb6aa0493e4a2b7
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-16 10:11:39 -0700

    add event emitters

--- src/app/keg-list/keg-list.component.ts
+++ src/app/keg-list/keg-list.component.ts
@@ -9,10 +9,35 @@ import { Keg } from '../models/keg.model';
 })
 export class KegListComponent {
   @Input() kegList: Keg[];
-  @Output() clickSender = new EventEmitter();
+  @Output() editKegSender = new EventEmitter();
+  @Output() soldPintSender = new EventEmitter();
+  @Output() soldGrowlerSender = new EventEmitter();
+  @Output() soldLargeGrowlerSender = new EventEmitter();
 
   editButtonClicked(kegToEdit: Keg) {
-    this.clickSender.emit(kegToEdit);
+    this.editKegSender.emit(kegToEdit);
+  }
+
+  sellPint(keg: Keg) {
+    this.soldPintSender.emit(keg);
+  }
+
+  sellGrowler(keg: Keg) {
+    this.soldGrowlerSender.emit(keg);
+  }
+
+  sellLargeGrowler(keg: Keg) {
+    this.soldLargeGrowlerSender.emit(keg);
+  }
+
+  priceColor(keg) {
+    if (keg.price > 5) {
+      return "expensive";
+    }
+    else if (keg.price >= 3 && keg.price <= 5) {
+      return "moderate";
+    }
+    else return "cheap";
   }
 
   constructor() { }
