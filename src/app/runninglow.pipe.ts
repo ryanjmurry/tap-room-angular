205c61d019b76a85816dd7d6d08b1f69f3024b18
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-16 10:31:42 -0700

    add running low pipe and transform logic

---
+++ src/app/runninglow.pipe.ts
@@ -0,0 +1,19 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import { Keg } from './models/keg.model';
+
+@Pipe({
+  name: "runninglow",
+  pure: false
+})
+
+export class RunningLowPipe implements PipeTransform {
+  transform(input: Keg[]) {
+    var output: Keg[];
+    for (let i = 0; i < input.length; i++) {
+      if (input[i].pintsRemaining <= 25) {
+        output.push(input[i]);
+      }
+    }
+    return output;
+  }
+}
