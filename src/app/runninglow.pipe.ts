ebb014dd0c25df4b737bd5e66455ccc0f086dc13
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-16 11:06:12 -0700

    fix pipe bug and add additional logic

--- src/app/runninglow.pipe.ts
+++ src/app/runninglow.pipe.ts
@@ -8,12 +8,12 @@ import { Keg } from './models/keg.model';
 
 export class RunningLowPipe implements PipeTransform {
   transform(input: Keg[]) {
-    var output: Keg[];
+    var output: Keg[] = [];
     for (let i = 0; i < input.length; i++) {
-      if (input[i].pintsRemaining <= 25) {
+      if (input[i].pintsRemaining <= 25 && input[i].pintsRemaining > 0) {
         output.push(input[i]);
       }
     }
-    return output;
+    return output
   }
 }
