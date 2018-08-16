202e4861480aa6a8f62be805c6423f0388d4d2db
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-16 11:06:59 -0700

    add input statement

--- src/app/running-low/running-low.component.ts
+++ src/app/running-low/running-low.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Keg } from '../models/keg.model';
 
 @Component({
   selector: 'app-running-low',
   templateUrl: './running-low.component.html',
   styleUrls: ['./running-low.component.css']
 })
-export class RunningLowComponent implements OnInit {
+export class RunningLowComponent {
+  @Input() runningLowList: Keg[];
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
