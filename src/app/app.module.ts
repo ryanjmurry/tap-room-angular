d35b8ca208ac153a174188c24642a1f24bf09941
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-16 13:48:18 -0700

    add font awesome package and change img to fa icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { KegListComponent } from './keg-list/keg-list.component';
@@ -21,7 +22,8 @@ import { RunningLowPipe } from './runninglow.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
