58e5efb8f97ac15b4807c2776c37e3fa476073fa
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-16 10:12:16 -0700

    add class methods to decrement pints by one, two, or four

--- src/app/models/keg.model.ts
+++ src/app/models/keg.model.ts
@@ -1,4 +1,22 @@
 export class Keg {
   public pintsRemaining: number = 124;
-  constructor(public beerName: string, public brewery: string, public price: number, public abv: number) { }
+  constructor(public beerName: string, public brewery: string, public style: string, public price: number, public abv: number) { }
+
+  decrementPintByOne() {
+    if (this.pintsRemaining > 0) {
+      this.pintsRemaining --;
+    }
+  }
+
+  decrementPintByTwo() {
+    if (this.pintsRemaining > 1) {
+      this.pintsRemaining -= 2;
+    }
+  }
+
+  decrementPintByFour() {
+    if (this.pintsRemaining > 3) {
+      this.pintsRemaining -= 4;
+    }
+  }
 }
