ceb96b7f3b8b380adf29455fb49d7062624daefe
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-16 15:43:42 -0700

    edit mock beer list

--- src/app/models/mock-kegs.ts
+++ src/app/models/mock-kegs.ts
@@ -1,8 +1,12 @@
 import { Keg } from './keg.model';
 //constructor(public beerName: string, public brewery: string, public price: number, public abv: number
 export const KEGS: Keg[] = [
-  new Keg("Lagunitas IPA", "Lagunitas", "IPA", 5, 6.2),
-  new Keg("Fresh Squeezed IPA", "Deschutes", "IPA", 6, 6.4),
-  new Keg("Rainier Lager", "Rainier Brewing", "Lager", 2, 4.6),
-  new Keg("Oatmeal Stout", "Firestone Walker", "Stout", 7, 5.5)
+  new Keg('Thad\'s Revenge', 'Red Bull HQ', 'Battery Acid', 5.50, 0.0),
+  new Keg('Burrito Slayer', 'The Chicken Coop', 'Gose', 11.00, 4.5),
+  new Keg('The disAbeler', 'Poke Bowl Brewing Co', 'Quadruble IPA', 9.00, 16.0),
+  new Keg('Fruit of the Earth', 'Local Oregonian Brew Co', 'Plum Ale', 1.50, 2.4),
+  new Keg('Basic and Boujee', 'Sass Ale Company', 'Light Pilsner', 2.50, 5.3),
+  new Keg('Get Forked Up', 'Nate Dog Brewing', 'Red Ale', 6.00, 4.3),
+  new Keg('Protein Pilsner', 'Boulders and Beer', 'Strong Pilsner', 6.00, 6.5),
+  new Keg('Chef\'s Special', 'Frazier and Sons', 'Imperial IPA', 5.50, 5.7)
  ];
