ddfc43a97609ef6655dae93012c1306f8c9293b6
Elly Maimon and Ryan Murry <elly+ryan@>
2018-08-16 11:05:52 -0700

    remove whitespace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { KegListComponent } from './keg-list/keg-list.component';
 import { EditKegComponent } from './edit-keg/edit-keg.component';
